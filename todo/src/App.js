ddd4547366ab065326bfc716d29713d78f35a5cb
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-05-11 13:27:54 -0700

    set the reducer and state to a separate file

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,30 +1,21 @@
 import React, { useState, useReducer } from 'react';
-
+import { taskReducer, initialTasks } from "./reducers/taskReducer"
+// import initialTasks from "./reducers/taskReducer"
 import './App.css';
 
 
-const taskReducer = (state, action) => {
-  return state
-}
 
-const initialTask = {
-  taskName: "get groceries",
-  completed: false,
-  taskId: "task1",
-  
-}
 
 function App() {
 
-  const [tasks, setTasks] = useState([initialTask])
-  const [state, dispatch] = useReducer(taskReducer, initialTask)
+  const [state, dispatch] = useReducer(taskReducer, initialTasks)
 
   return (
     <div className="App">
       <header>
         <h1>Tasks</h1>
       </header>
-      {tasks.map(task => {
+      {initialTasks.map(task => {
         return <p>{task.taskName}</p>
       })}
     </div>
---
+++ todo/src/reducers/taskReducer.js
@@ -0,0 +1,11 @@
+export const taskReducer = (state, action) => {
+  return state;
+};
+
+export const initialTasks = [
+  {
+    taskName: "get groceries",
+    completed: false,
+    taskId: "task1",
+  },
+];
